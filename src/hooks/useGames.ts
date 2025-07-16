f3d2a1299525be15af5810b7af3aa57962044b90
Joseph Neil Gapuz <josephneil873@gmail.com>
2025-07-16 16:09:37 +0800

    Create a generic data fetching hook

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import useData from "./useData";
 
 /** Represents a platform. */
 export interface Platform {
@@ -17,43 +15,10 @@ export interface Game {
   metacritic: number;
 }
 
-/** Response format for the GET /games API call. */
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
-
 /**
- * @description Custom hook to fetch and return list of games from API.
- * - Cancels request on unmount using AbortController.
- * - Returns error message if request fails.
- * @returns An object containing the list of games and an error message if any.
+ * Custom hook to fetch games list from API.
+ * @returns Game, loading, error state.
  */
-const useGames = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const controller = new AbortController();
-
-    setLoading(true);
-    apiClient
-      .get<FetchGamesResponse>("/games", { signal: controller.signal })
-      .then((response) => {
-        setGames(response.data.results);
-        setLoading(false);
-      })
-      .catch((error) => {
-        if (error instanceof CanceledError) return;
-        setError(error.message);
-        setLoading(false);
-      })
-
-    return () => controller.abort();
-  }, []);
-
-  return { games, error, isLoading };
-};
+const useGames = () => useData<Game>("/games");
 
 export default useGames;
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,49 +1,15 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import useData from "./useData";
 
 /** Represents a single genre object. */
-interface Genre {
+export interface Genre {
   id: number;
   name: string;
 }
 
-/** Response structure fro GET /genres API call. */
-interface FetchGenresResponse {
-  count: number;
-  results: Genre[];
-}
-
 /**
- * Custom hook to fetch game genres.
- * 
- * @returns An object with genres. 
+ * Custom hook to fetch genres.
+ * @returns Genre, loading, error state.
  */
-const useGenres = () => {
-  const [genres, setGenres] = useState<Genre[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const controller = new AbortController();
-
-    setLoading(true);
-    apiClient
-      .get<FetchGenresResponse>("/genres", { signal: controller.signal })
-      .then((response) => {
-        setGenres(response.data.results);
-        setLoading(false);
-      })
-      .catch((error) => {
-        if (error instanceof CanceledError) return;
-        setError(error.message);
-        setLoading(false);
-      });
-
-    return () => controller.abort();
-  }, []);
-
-  return { genres, error, isLoading };
-};
+const useGenres = () => useData<Genre>("/genres");
 
 export default useGenres;
