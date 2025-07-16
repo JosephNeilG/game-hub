4fbe2b2f098b3a83a20e026cdaed3fceb03d8fb1
Joseph Neil Gapuz <josephneil873@gmail.com>
2025-07-16 10:12:53 +0800

    Add comment

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,25 +2,36 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { Text } from "@chakra-ui/react";
 
+/** Represents a single game from the API. */
 interface Game {
   id: number;
   name: string;
 }
+
+/** Response format for the GET /games API call. */
 interface FetchGamesResponse {
   count: number;
   results: Game[];
 }
 
+/**
+ * Fetches and displays a list of games from RAWG API.
+ *
+ * - Uses axis to send a GET request.
+ * - Shows error if request fails.
+ * - Maps the game list.
+ */
 const GameGrid = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
 
   useEffect(() => {
     apiClient
-      .get<FetchGamesResponse>("/gaxmes")
+      .get<FetchGamesResponse>("/games")
       .then((response) => setGames(response.data.results))
       .catch((error) => setError(error.message));
   });
+
   return (
     <>
       {error && <Text>{error}</Text>}
