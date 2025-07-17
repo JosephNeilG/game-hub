f5418279e271dac4c4f1f6f5776d88cb90b66561
Joseph Neil Gapuz <josephneil873@gmail.com>
2025-07-17 10:54:13 +0800

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,11 +1,13 @@
+import noImage from "../assets/no-image-placeholder.webp";
+
 /**
- * Inserts a crop rule into the image URL to resize it to 600x400.
+ * Inserts a crop rule into the image URL to resize it to 600x400. If no available image, returns a default placeholder image.
  * @param url - The original image URL.
- * @returns A modified URL with cropping rule.
+ * @returns Cropped image URL, or placeholdr if no image available.
  */
 
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return noImage;
 
   const target = "media/";
   const index = url.indexOf(target) + target.length;
