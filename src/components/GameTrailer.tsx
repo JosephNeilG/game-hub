7c4445859d23c45a97c4c875a8f4ff80d4f17091
Joseph Neil Gapuz <josephneil873@gmail.com>
2025-07-24 13:06:45 +0800

    Build game trailer

---
+++ src/components/GameTrailer.tsx
@@ -0,0 +1,26 @@
+import useTrailers from "../hooks/useTrailers";
+
+interface Props {
+  gameId: number;
+}
+
+/** Component to show the trailer of a game. */
+const GameTrailer = ({ gameId }: Props) => {
+  // fetch trailers using custom hook
+  const { data, isLoading, error } = useTrailers(gameId);
+  data?.count !== 0 ? console.log(data) : console.log("no trailer available");
+
+  if (isLoading) return null;
+
+  if (error) throw error;
+
+  // get first trailer from the result list
+  const first = data?.results[0];
+
+  // if exists, render video. otherwise, return null
+  return first ? (
+    <video src={first.data[480]} poster={first.preview} controls />
+  ) : null;
+};
+
+export default GameTrailer;
