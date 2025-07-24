3e8fab8c9eafd0fcbf9dd40050b1090b911796de
Joseph Neil Gapuz <josephneil873@gmail.com>
2025-07-24 14:20:26 +0800

    Fix navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,12 +2,15 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
 
 /** Navbar conaining logo, search bar, and color mode toggle. */
 const NavBar = () => {
   return (
-    <HStack padding="10px">
-      <Image src={logo} boxSize="60px" />
+    <HStack paddingX={5} paddingY={2}>
+      <Link to="/">
+        <Image src={logo} boxSize="60px" objectFit="cover" />
+      </Link>
       <SearchInput />
       <ColorModeSwitch></ColorModeSwitch>
     </HStack>
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,6 +2,7 @@ import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 import useGameQueryStore from "../store";
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
   const ref = useRef<HTMLInputElement>(null);
@@ -9,12 +10,17 @@ const SearchInput = () => {
   // get the function to update global search text in zustand store
   const setSearchText = useGameQueryStore((s) => s.setSearchText);
 
+  const navigate = useNavigate();
+
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
         // if ref is pointing to a valid input field, update global state
-        if (ref.current) setSearchText(ref.current.value);
+        if (ref.current) {
+          setSearchText(ref.current.value);
+          navigate("/");
+        }
       }}
     >
       <InputGroup>
